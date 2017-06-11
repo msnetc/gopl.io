7174bfee1e8d1e3132e9e3fadfc9f188a6cf30f1
jerry <jerrymiao@renqidai.cc>
2017-06-11 21:48:56 +0800

    add

---
+++ ch8/clock3/clock.go
@@ -0,0 +1,37 @@
+package main
+
+//writtin by jerry miao
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main( )  {
+	 listener, err := net.Listen("tcp", "localhost:8000")
+	if(err != nil){
+		log.Fatal(err)
+	}
+
+	for{
+		conn, err := listener.Accept();
+		if err != nil{
+			log.Println(err);
+			continue;
+		}
+		go handConn(conn)
+	}
+}
+
+func handConn( c net.Conn)  {
+	defer  c.Close();
+	for{
+		_, err := io.WriteString(c, time.Now().Format("2006-01-02 03:04:05 \n"))
+		if err != nil{
+			log.Println(err);
+			continue;
+		}
+		time.Sleep(1 * time.Second);
+	}
+}
